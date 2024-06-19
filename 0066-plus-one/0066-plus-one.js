3cdfa97c6d0b07abb81a545b0e78443d05d8faca
Chynarbekov_eee <142959677+elizar07@users.noreply.github.com>
2024-06-19 19:09:06 +0600

    Time: 41 ms (96.72%), Space: 48584000 (81.63%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+let plusOne = function(digits) {
+    let i = digits.length - 1;
+
+    while (i >= 0) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+        i--;
+    }
+
+    digits.unshift(1);
+    return digits;
+};
+
