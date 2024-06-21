127b7d4d1f919cb541e6a53bdf7a7a064355efb6
Chynarbekov_eee <142959677+elizar07@users.noreply.github.com>
2024-06-21 17:02:53 +0600

    Time: 103 ms (22.31%), Space: 57396000 (85.40%) - LeetHub

---
+++ 0860-design-circular-queue/0860-design-circular-queue.js
@@ -0,0 +1,77 @@
+/**
+ * @param {number} k
+ */
+var MyCircularQueue = function(k) {
+    this.queue = new Array(k);
+    this.head = -1;
+    this.tail = -1;
+    this.size = k;
+};
+
+/** 
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function(value) {
+    if (this.isFull()) {
+        return false;
+    }
+    if (this.isEmpty()) {
+        this.head = 0;
+    }
+    this.tail = (this.tail + 1) % this.size;
+    this.queue[this.tail] = value;
+    return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function() {
+    if (this.isEmpty()) {
+        return false;
+    }
+    if (this.head === this.tail) {
+        this.head = -1;
+        this.tail = -1;
+    } else {
+        this.head = (this.head + 1) % this.size;
+    }
+    return true;
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function() {
+    if (this.isEmpty()) {
+        return -1;
+    }
+    return this.queue[this.head];
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function() {
+    if (this.isEmpty()) {
+        return -1;
+    }
+    return this.queue[this.tail];
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function() {
+    return this.head === -1;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function() {
+    return ((this.tail + 1) % this.size) === this.head;
+};
+
+
